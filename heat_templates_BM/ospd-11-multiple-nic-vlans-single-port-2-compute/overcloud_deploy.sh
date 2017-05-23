8b272171d76af40e3498a1353263a5ac609daca0
Eran Kuris <ekuris@redhat.com>
2017-05-23 17:02:32 +0300

    remove old
    Change-Id: I453aa3025627b08217cb4ea6eca76cb08da301d5

--- heat_templates_BM/ospd-11-multiple-nic-vlans-single-port-2-compute/overcloud_deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-openstack overcloud deploy \
---templates \
---ntp-server clock.redhat.com \
--e /home/stack/ospd-11-multiple-nic-vlans-single-port-2-compute/network-environment.yaml \
---log-file overcloud_install.log &> overcloud_install.log
