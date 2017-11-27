e4addd7852abc5f2f791a614c4325e6fbbabc842
ekuris-redhat <ekuris@redhat.com>
2017-11-27 09:45:43 +0200

    Fix templates
    Change-Id: I97b58d51f36c8c2fdc8b4fbc8720797a70030d8b

--- BM_heat_template/ospd-11-multiple-nic-vlans-sriov-hybrid-ha/overcloud_deploy.sh
+++ BM_heat_template/ospd-11-multiple-nic-vlans-sriov-hybrid-ha/overcloud_deploy.sh
@@ -3,7 +3,7 @@
 
 openstack overcloud deploy  \
 --templates \
--r /home/stack/ospd-11-multiple-nic-vlans-sriov-hybrid-ha/roles-data.yaml \
+-r /home/stack/ospd-11-multiple-nic-vlans-sriov-hybrid-ha/roles_data.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-sriov.yaml \
 -e /home/stack/ospd-11-multiple-nic-vlans-sriov-hybrid-ha/network-environment.yaml \
--- BM_heat_template/ospd-12-multiple-nic-vlans-sriov-hybrid-ha/overcloud_deploy.sh
+++ BM_heat_template/ospd-12-multiple-nic-vlans-sriov-hybrid-ha/overcloud_deploy.sh
@@ -4,7 +4,7 @@
 openstack overcloud deploy  \
 --templates \
 --timeout 120 \
--r /home/stack/ospd-12-multiple-nic-vlans-sriov-hybrid-ha/roles-data.yaml \
+-r /home/stack/ospd-12-multiple-nic-vlans-sriov-hybrid-ha/roles_data.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-sriov.yaml \
