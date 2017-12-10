65410ba73e1824a746d8304c2e0d26dedc7aac5f
ekuris-redhat <ekuris@redhat.com>
2017-12-10 11:38:16 +0200

    Editing sriov templates
    Change-Id: If4d1eaa64bd80d38e5eea4901ef1263793c99d68

--- BM_heat_template/ospd-11-vlan-multiple-nic-sriov-hybrid-ha/overcloud_deploy.sh
+++ BM_heat_template/ospd-11-vlan-multiple-nic-sriov-hybrid-ha/overcloud_deploy.sh
@@ -3,8 +3,8 @@
 
 openstack overcloud deploy  \
 --templates \
--r /home/stack/ospd-11-multiple-nic-vlans-sriov-hybrid-ha/roles_data.yaml \
+-r /home/stack/ospd-11-vlan-multiple-nic-sriov-hybrid-ha/roles_data.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-sriov.yaml \
--e /home/stack/ospd-11-multiple-nic-vlans-sriov-hybrid-ha/network-environment.yaml \
+-e /home/stack/ospd-11-vlan-multiple-nic-sriov-hybrid-ha/network-environment.yaml \
 --log-file overcloud_install.log &> overcloud_install.log
--- BM_heat_template/ospd-12-vlan-multiple-nic-sriov-hybrid-ha-ovn/overcloud_deploy.sh
+++ BM_heat_template/ospd-12-vlan-multiple-nic-sriov-hybrid-ha-ovn/overcloud_deploy.sh
@@ -4,10 +4,11 @@
 openstack overcloud deploy  \
 --templates \
 --timeout 120 \
--r /home/stack/ospd-12-multiple-nic-vlans-sriov-hybrid-ha/roles_data.yaml \
+-r /home/stack/ospd-12-vlan-multiple-nic-sriov-hybrid-ha-ovn/roles_data.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-sriov.yaml \
--e /home/stack/ospd-12-multiple-nic-vlans-sriov-hybrid-ha/docker-images.yaml \
--e /home/stack/ospd-12-multiple-nic-vlans-sriov-hybrid-ha/network-environment.yaml \
+-e /home/stack/ospd-12-vlan-multiple-nic-sriov-hybrid-ha-ovn/docker-images.yaml \
+-e /home/stack/ospd-12-vlan-multiple-nic-sriov-hybrid-ha-ovn/network-environment.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ml2-ovn-ha.yaml \
 --log-file overcloud_install.log &> overcloud_install.log
