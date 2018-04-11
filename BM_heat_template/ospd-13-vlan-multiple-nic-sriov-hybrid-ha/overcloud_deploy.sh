213d6a541cdcb7a6db22faaf98420279dea01eca
Eran Kuris <ekuris@redhat.com>
2018-04-11 11:10:59 +0300

    Adding OSP13 sriov templates
    Change-Id: Ifb659b1859ee51299456f99c743a4bf616d00eb7

---
+++ BM_heat_template/ospd-13-vlan-multiple-nic-sriov-hybrid-ha/overcloud_deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+openstack overcloud deploy  \
+--templates \
+--timeout 120 \
+-r /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha/roles_data.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/neutron-sriov.yaml \
+-e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha/docker-images.yaml \
+-e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha/network-environment.yaml \
+--log-file overcloud_install.log &> overcloud_install.log
