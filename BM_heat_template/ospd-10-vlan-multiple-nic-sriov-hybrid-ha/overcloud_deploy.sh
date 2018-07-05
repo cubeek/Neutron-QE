a53b7de36e9f7f6cbae3255e411e63d6c4ae0551
Eran Kuris <ekuris@redhat.com>
2018-07-05 10:11:50 +0300

    Updated OSP10 THT with SRIOV composble role
    Change-Id: I30ae028aabee4c190e9cd93f8ea7f1e187e9eae3

--- BM_heat_template/ospd-10-vlan-multiple-nic-sriov-hybrid-ha/overcloud_deploy.sh
+++ BM_heat_template/ospd-10-vlan-multiple-nic-sriov-hybrid-ha/overcloud_deploy.sh
@@ -4,7 +4,7 @@
 openstack overcloud deploy  \
 --templates \
 -r /home/stack/ospd-10-vlan-multiple-nic-sriov-hybrid-ha/roles_data.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-sriov.yaml \
 -e /home/stack/ospd-10-vlan-multiple-nic-sriov-hybrid-ha/network-environment.yaml \
 --log-file overcloud_install.log &> overcloud_install.log
+
