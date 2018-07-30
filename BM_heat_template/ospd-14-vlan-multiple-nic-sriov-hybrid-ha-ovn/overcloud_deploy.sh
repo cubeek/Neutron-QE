7a82fcb06475e0928b8afad41c9672a24a7c2005
Eran Kuris <ekuris@redhat.com>
2018-07-30 09:08:48 +0300

    Added SRIOV THT for OSP14-OVN
    Change-Id: I241d908dc95bfa18fd0c3a45bf3759ca72ebe21a

---
+++ BM_heat_template/ospd-14-vlan-multiple-nic-sriov-hybrid-ha-ovn/overcloud_deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+openstack overcloud deploy  \
+--templates \
+--timeout 120 \
+-r /home/stack/ospd-14-vlan-multiple-nic-sriov-hybrid-ha-ovn/roles_data.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services-docker/neutron-sriov.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
+-e /home/stack/ospd-14-vlan-multiple-nic-sriov-hybrid-ha-ovn/docker-images.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovn-ha.yaml \
+-e /home/stack/ospd-14-vlan-multiple-nic-sriov-hybrid-ha-ovn/network-environment.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovn-sriov.yaml \
+--log-file overcloud_install.log &> overcloud_install.log
