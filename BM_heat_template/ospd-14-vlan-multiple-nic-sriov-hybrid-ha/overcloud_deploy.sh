2b3c8681b1e9463599e266881472f07860de94ac
ekuris <ekuris@redhat.com>
2018-08-29 09:47:27 +0300

    Add OSP14 SRIOV THT files.
    Change-Id: Ie0e9160a4ef57bb08528f8c0111babb4deb41c6f

---
+++ BM_heat_template/ospd-14-vlan-multiple-nic-sriov-hybrid-ha/overcloud_deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+openstack overcloud deploy  \
+--templates \
+--timeout 120 \
+-r /home/stack/ospd-14-vlan-multiple-nic-sriov-hybrid-ha/roles_data.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
+-e /home/stack/ospd-14-vlan-multiple-nic-sriov-hybrid-ha/docker-images.yaml \
+-e /home/stack/ospd-14-vlan-multiple-nic-sriov-hybrid-ha/network-environment.yaml \
+--log-file overcloud_install.log &> overcloud_install.log
