918b762ad04307ca5a40cf3dabd72bd2daf0c11f
Roee Agiman <ragiman@redhat.com>
2018-11-07 11:10:13 +0200

    Fixed overcloud_deploy.sh for ospd14 SRIOV hybrid setup job
    Change-Id: Ib07eb7f2f80c9f86e672239cf715940cc2bbdef8

--- BM_heat_template/ospd-14-vlan-multiple-nic-sriov-hybrid-ha/overcloud_deploy.sh
+++ BM_heat_template/ospd-14-vlan-multiple-nic-sriov-hybrid-ha/overcloud_deploy.sh
@@ -5,10 +5,8 @@ openstack overcloud deploy  \
 --timeout 120 \
 -r /home/stack/ospd-14-vlan-multiple-nic-sriov-hybrid-ha/roles_data.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
--e /home/stack/ospd-14-vlan-multiple-nic-sriov-hybrid-ha/docker-images.yaml \
+-e /home/stack/containers-prepare-parameter.yaml \
 -e /home/stack/ospd-14-vlan-multiple-nic-sriov-hybrid-ha/network-environment.yaml \
 --log-file overcloud_install.log &> overcloud_install.log
