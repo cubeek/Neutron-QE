e0df4ef661114895dac377d4ee7b3e56d36c3cde
ekuris <ekuris@redhat.com>
2019-02-27 13:55:35 +0200

    Update oosp13 ovn THT files
    Change-Id: Ic3daf24bc2a93c2cc9c2740808fed15c9c5d0953

--- BM_heat_template/ospd-13-vlan-sriov-hybrid-ha-ovn-squad/overcloud_deploy.sh
+++ BM_heat_template/ospd-13-vlan-sriov-hybrid-ha-ovn-squad/overcloud_deploy.sh
@@ -4,12 +4,15 @@
 openstack overcloud deploy  \
 --templates \
 --timeout 120 \
--r /home/stack/ospd-13-vlan-sriov-hybrid-ha-ovn-squad/roles_data.yaml \
+-r /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ovn/roles_data.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
--e /home/stack/ospd-13-vlan-sriov-hybrid-ha-ovn-squad/docker-images.yaml \
--e /home/stack/ospd-13-vlan-sriov-hybrid-ha-ovn-squad/network-environment.yaml \
+-e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ovn/docker-images.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovn-ha.yaml \
+-e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ovn/network-environment.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovn-sriov.yaml \
 --log-file overcloud_install.log &> overcloud_install.log
+
