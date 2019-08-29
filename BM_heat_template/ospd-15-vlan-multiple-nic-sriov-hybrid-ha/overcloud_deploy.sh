4b81bb16fc3b9ead560a5f6a4c38a70050a2db2e
ekuris <ekuris@redhat.com>
2019-08-29 14:24:33 +0300

    changed order of ymal files.
    Change-Id: I2b15ef443ac1b298da37b1da83dc5d7f37928e04

--- BM_heat_template/ospd-15-vlan-multiple-nic-sriov-hybrid-ha/overcloud_deploy.sh
+++ BM_heat_template/ospd-15-vlan-multiple-nic-sriov-hybrid-ha/overcloud_deploy.sh
@@ -8,11 +8,11 @@ fi
 openstack -vvv overcloud deploy \
 --templates \
 --timeout 120 \
--e /home/stack/ospd-15-vlan-multiple-nic-sriov-hybrid-ha/network-environment.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
+-e /home/stack/ospd-15-vlan-multiple-nic-sriov-hybrid-ha/network-environment.yaml \
 -e /home/stack/containers-prepare-parameter.yaml \
 -r $THT_PATH/roles_data.yaml \
 --log-file overcloud_install.log &> overcloud_install.log
