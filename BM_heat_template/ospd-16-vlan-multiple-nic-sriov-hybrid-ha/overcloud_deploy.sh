dbba83182cd18340c0299986818c36269fa537a4
ccamposr <ccamposr@redhat.com>
2019-12-12 15:02:40 +0100

    [vnes-ci-sriov]Adjust network config
    Change-Id: I3f81c27ad2d12d73c3724f0e4bcb76cc3b85466d

--- BM_heat_template/ospd-16-vlan-multiple-nic-sriov-hybrid-ha/overcloud_deploy.sh
+++ BM_heat_template/ospd-16-vlan-multiple-nic-sriov-hybrid-ha/overcloud_deploy.sh
@@ -14,5 +14,6 @@ openstack -vvv overcloud deploy \
 -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
 -e $THT_PATH/network-environment.yaml \
 -e /home/stack/containers-prepare-parameter.yaml \
+-e $THT_PATH/os-net-config-mappings.yaml \
 -r $THT_PATH/roles_data.yaml \
 --log-file overcloud_install.log &> overcloud_install.log
