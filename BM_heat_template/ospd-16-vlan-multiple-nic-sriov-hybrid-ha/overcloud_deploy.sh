b1529969f3f3cc7cb6dd7cdefb85c5ff9b4224a2
ccamposr <ccamposr@redhat.com>
2020-02-05 15:44:58 +0100

    [vnes-ci-sriov-update]Avoid use to bash variable in the file paths
      overcloud_deploy.sh paths are copied to create other scripts
     and they go to fail if the path includes variables
    
    Change-Id: I492cdaf250be1f773d663cb4e7c3c20f24370e42

--- BM_heat_template/ospd-16-vlan-multiple-nic-sriov-hybrid-ha/overcloud_deploy.sh
+++ BM_heat_template/ospd-16-vlan-multiple-nic-sriov-hybrid-ha/overcloud_deploy.sh
@@ -12,8 +12,8 @@ openstack -vvv overcloud deploy \
 -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
--e $THT_PATH/network-environment.yaml \
+-e /home/stack/ospd-16-vlan-multiple-nic-sriov-hybrid-ha/network-environment.yaml \
 -e /home/stack/containers-prepare-parameter.yaml \
--e $THT_PATH/os-net-config-mappings.yaml \
--r $THT_PATH/roles_data.yaml \
+-e /home/stack/ospd-16-vlan-multiple-nic-sriov-hybrid-ha/os-net-config-mappings.yaml \
+-r /home/stack/ospd-16-vlan-multiple-nic-sriov-hybrid-ha/roles_data.yaml \
 --log-file overcloud_install.log &> overcloud_install.log
