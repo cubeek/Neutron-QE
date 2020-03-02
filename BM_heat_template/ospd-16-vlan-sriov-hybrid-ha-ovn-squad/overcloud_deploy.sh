398050297b39d17fede5c02834e104ec49d91fec
Lucas Gomez Martins <lmartins@redhat.com>
2020-03-02 10:28:06 +0000

    [OVN] Add templates for osp16 OVN SR-IOV setup
    This patch is adding the templates for setting up OVN with SR-IOV on OSP
    16.
    
    Change-Id: I6a5e006c7da1ed28a2cfca20d3b6abebe9ffb843
    Signed-off-by: Lucas Alvares Gomes <lucasagomes@gmail.com>

---
+++ BM_heat_template/ospd-16-vlan-sriov-hybrid-ha-ovn-squad/overcloud_deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+THT_PATH='/home/stack/ospd-16-vlan-sriov-hybrid-ha-ovn-squad'
+
+if [[ ! -f "$THT_PATH/roles_data.yaml" ]]; then
+  openstack overcloud roles generate -o $THT_PATH/roles_data.yaml Controller ComputeSriov
+fi
+
+openstack -vvv overcloud deploy  \
+--templates \
+--timeout 120 \
+-r $THT_PATH/roles_data.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovn-ha.yaml \
+-e /home/stack/ospd-16-vlan-sriov-hybrid-ha-ovn-squad/network-environment.yaml \
+-e /home/stack/containers-prepare-parameter.yaml \
+--log-file overcloud_install.log &> overcloud_install.log
