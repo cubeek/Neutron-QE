60bed6688b7683f84da3442052f026e60d40ab91
ccamposr <ccamposr@redhat.com>
2020-05-10 01:04:35 +0200

    [vnes-ci-sriov-update]Configure full path template
    Overcloud update prepare is failing becasue the environment
    variable is not included.
    
    Change-Id: Ic727880b5626ce7fe98564a27f267f94dacb45fc

--- BM_heat_template/ospd-16-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan9/overcloud_deploy.sh
+++ BM_heat_template/ospd-16-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan9/overcloud_deploy.sh
@@ -8,13 +8,13 @@ fi
 openstack -vvv overcloud deploy  \
 --templates \
 --timeout 120 \
--r $THT_PATH/roles_data.yaml \
+-r /home/stack/ospd-16-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan9/roles_data.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
--e $THT_PATH/network-environment.yaml \
+-e /home/stack/ospd-16-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan9/network-environment.yaml \
 -e /home/stack/containers-prepare-parameter.yaml \
--e $THT_PATH//os-net-config-mappings.yaml \
+-e /home/stack/ospd-16-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan9/os-net-config-mappings.yaml \
 --log-file overcloud_install.log &> overcloud_install.log
