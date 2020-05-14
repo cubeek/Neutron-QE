79fc6736adf2960e452af5abe9ac64168c956610
ccamposr <ccamposr@redhat.com>
2020-05-14 12:51:35 +0200

    [vnes-ci-sriov-update]Configure full path template
    Change-Id: I7932d1c018a0447291ea9033d7ad41b73cbd14c2

--- BM_heat_template/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan9/overcloud_deploy.sh
+++ BM_heat_template/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan9/overcloud_deploy.sh
@@ -8,14 +8,14 @@ fi
 openstack -vvv overcloud deploy  \
 --templates \
 --timeout 120 \
--r $THT_PATH/roles_data.yaml \
+-r /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan9/roles_data.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml \
--e $THT_PATH/network-environment.yaml \
--e $THT_PATH/docker-images.yaml \
--e $THT_PATH//os-net-config-mappings.yaml \
+-e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan9/network-environment.yaml \
+-e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan9/docker-images.yaml \
+-e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan9/os-net-config-mappings.yaml \
 --log-file overcloud_install.log &> overcloud_install.log
