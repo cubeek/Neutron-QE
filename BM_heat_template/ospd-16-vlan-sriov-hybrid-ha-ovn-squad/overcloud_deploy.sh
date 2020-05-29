7b8b4b634356426962deeda1e0bc2b014ef585c3
Eduardo Olivares <eolivare@redhat.com>
2020-05-29 12:54:53 +0200

    Remove env variable THT_PATH from SRIOV OVN overcloud_deploy.sh
    Environment files from overcloud_deploy.sh are extracted by infrared at
    Overcloud Update stage to generate overcloud_update_prepare.sh script.
    Paths based on environment variables are not supported by that infrared
    task
    
    Change-Id: Ic6d6d0c43b87d6d88806ec9f9228b68cbff0dba4

--- BM_heat_template/ospd-16-vlan-sriov-hybrid-ha-ovn-squad/overcloud_deploy.sh
+++ BM_heat_template/ospd-16-vlan-sriov-hybrid-ha-ovn-squad/overcloud_deploy.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
-THT_PATH='/home/stack/ospd-16-vlan-sriov-hybrid-ha-ovn-squad'
-
-if [[ ! -f "$THT_PATH/roles_data.yaml" ]]; then
-  openstack overcloud roles generate -o $THT_PATH/roles_data.yaml ControllerSriov ComputeSriov
+if [[ ! -f "/home/stack/ospd-16-vlan-sriov-hybrid-ha-ovn-squad/roles_data.yaml" ]]; then
+  openstack overcloud roles generate -o /home/stack/ospd-16-vlan-sriov-hybrid-ha-ovn-squad/roles_data.yaml ControllerSriov ComputeSriov
 fi
 
 openstack -vvv overcloud deploy  \
 --templates \
 --timeout 120 \
--r $THT_PATH/roles_data.yaml \
+-r /home/stack/ospd-16-vlan-sriov-hybrid-ha-ovn-squad/roles_data.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovn-dvr-ha.yaml \
 -e /home/stack/containers-prepare-parameter.yaml \
--e $THT_PATH/api-policies.yaml \
--e $THT_PATH/network-environment.yaml \
--e $THT_PATH/os-net-config-mappings.yaml \
--e $THT_PATH/nova-resize-on-the-same-host.yaml \
--e $THT_PATH/l3_fip_qos.yaml \
--e $THT_PATH/ovn-extras.yaml \
+-e /home/stack/ospd-16-vlan-sriov-hybrid-ha-ovn-squad/api-policies.yaml \
+-e /home/stack/ospd-16-vlan-sriov-hybrid-ha-ovn-squad/network-environment.yaml \
+-e /home/stack/ospd-16-vlan-sriov-hybrid-ha-ovn-squad/os-net-config-mappings.yaml \
+-e /home/stack/ospd-16-vlan-sriov-hybrid-ha-ovn-squad/nova-resize-on-the-same-host.yaml \
+-e /home/stack/ospd-16-vlan-sriov-hybrid-ha-ovn-squad/l3_fip_qos.yaml \
+-e /home/stack/ospd-16-vlan-sriov-hybrid-ha-ovn-squad/ovn-extras.yaml \
 --log-file overcloud_install.log &> overcloud_install.log
