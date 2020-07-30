bd3584042313c0f902e863207c38f4f8e817099c
ccamposr <ccamposr@redhat.com>
2020-07-30 09:54:25 +0200

    [vnes-ci-sriov]New templates for titan13 environment
    titan13 templates using routed lab network
    
    Change-Id: I994c1979306adfef6162d0b009a4c116a09a8b4f

---
+++ BM_heat_template/ospd-16-vlan-multiple-nic-sriov-hybrid-ha-titan13/overcloud_deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+THT_PATH='/home/stack/ospd-16-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan13'
+
+if [[ ! -f "$THT_PATH/roles_data.yaml" ]]; then
+  openstack overcloud roles generate -o $THT_PATH/roles_data.yaml Controller ComputeSriov
+fi
+
+openstack -vvv overcloud deploy  \
+--templates \
+--timeout 120 \
+-r /home/stack/ospd-16-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan13/roles_data.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
+-e /home/stack/ospd-16-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan13/network-environment.yaml \
+-e /home/stack/containers-prepare-parameter.yaml \
+-e /home/stack/ospd-16-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan13/os-net-config-mappings.yaml \
+--log-file overcloud_install.log &> overcloud_install.log
