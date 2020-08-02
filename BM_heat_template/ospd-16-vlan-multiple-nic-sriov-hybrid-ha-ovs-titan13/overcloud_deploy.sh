d0ec14cd6a651140f91a9210fcada9d42dcca9fc
ccamposr <ccamposr@redhat.com>
2020-08-02 11:29:39 +0200

    [vnes-ci-sriov]Add extra templates support in titan13 templates
    Add qos, dns, fip extra config for tempest tests
    
    Change-Id: I3369d5da61808c34ec0570cd294bcea6b4dd9f67

--- BM_heat_template/ospd-16-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan13/overcloud_deploy.sh
+++ BM_heat_template/ospd-16-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan13/overcloud_deploy.sh
@@ -14,6 +14,10 @@ openstack -vvv overcloud deploy  \
 -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
+-e /home/stack/ospd-16-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan13/l3_fip_qos.yaml \
+-e /home/stack/ospd-16-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan13/dns.yaml \
+-e /home/stack/ospd-16-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan13/floating_ip_port_forwarding.yaml \
+-e /home/stack/ospd-16-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan13/nova-resize-on-the-same-host.yaml \
 -e /home/stack/ospd-16-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan13/network-environment.yaml \
 -e /home/stack/containers-prepare-parameter.yaml \
 -e /home/stack/ospd-16-vlan-multiple-nic-sriov-hybrid-ha-ovs-titan13/os-net-config-mappings.yaml \
