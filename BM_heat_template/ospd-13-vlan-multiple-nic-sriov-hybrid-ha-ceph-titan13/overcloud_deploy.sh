423ccc5601d395520ed48acc396a67f333550038
ccamposr <ccamposr@redhat.com>
2020-09-28 14:44:30 +0200

    [vnes-sriov]Fix path in overcloud.sh for osp13 ceph templates
    Change-Id: I6dcc2c167d2051a173175ff224c7cbac7f6de94f

--- BM_heat_template/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ceph-titan13/overcloud_deploy.sh
+++ BM_heat_template/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ceph-titan13/overcloud_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-THT_PATH='/home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-titan13'
+THT_PATH='/home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ceph-titan13'
 
 if [[ ! -f "$THT_PATH/roles_data.yaml" ]]; then
   openstack overcloud roles generate -o $THT_PATH/roles_data.yaml Controller ComputeSriov
@@ -8,16 +8,16 @@ fi
 openstack -vvv overcloud deploy  \
 --templates \
 --timeout 120 \
--r /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-titan13/roles_data.yaml \
+-r /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ceph-titan13/roles_data.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-sriov.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml \
--e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-titan13/l3_fip_qos.yaml \
--e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-titan13/dns.yaml \
--e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-titan13/nova-resize-on-the-same-host.yaml \
--e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-titan13/network-environment.yaml \
--e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-titan13/docker-images.yaml \
--e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-titan13/os-net-config-mappings.yaml \
+-e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ceph-titan13/l3_fip_qos.yaml \
+-e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ceph-titan13/dns.yaml \
+-e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ceph-titan13/nova-resize-on-the-same-host.yaml \
+-e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ceph-titan13/network-environment.yaml \
+-e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ceph-titan13/docker-images.yaml \
+-e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ceph-titan13/os-net-config-mappings.yaml \
 --log-file overcloud_install.log &> overcloud_install.log
