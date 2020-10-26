5cd81ad5b3474b2aa62eb189548cc67ba1fd1a4f
Roman Safronov <rsafrono@redhat.com>
2020-10-26 18:12:52 +0200

    Add OSP13 templates with ceph for titan09
    These templates will be used for OVS SRIOV FFU deployment.
    It is required for OVN migration testing on SRIOV environment
    after FFU from OSP13 to OSP16.1.
    
    Change-Id: If65bcb3cf7c0e4a023cbd94ef6021d65f453013b

---
+++ BM_heat_template/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ovs-ceph-titan9/overcloud_deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+THT_PATH='/home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ovs-ceph-titan9'
+
+if [[ ! -f "$THT_PATH/roles_data.yaml" ]]; then
+  openstack overcloud roles generate -o $THT_PATH/roles_data.yaml Controller ComputeSriov CephStorage
+fi
+
+openstack -vvv overcloud deploy  \
+--templates \
+--timeout 120 \
+-r /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ovs-ceph-titan9/roles_data.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/neutron-sriov.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml \
+-e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ovs-ceph-titan9/l3_fip_qos.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/cinder-backup.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-ansible.yaml \
+-e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ovs-ceph-titan9/dns.yaml \
+-e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ovs-ceph-titan9/nova-resize-on-the-same-host.yaml \
+-e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ovs-ceph-titan9/network-environment.yaml \
+-e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ovs-ceph-titan9/docker-images.yaml \
+-e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ovs-ceph-titan9/os-net-config-mappings.yaml \
+-e /home/stack/ospd-13-vlan-multiple-nic-sriov-hybrid-ha-ceph-titan9/ceph.yaml \
+--log-file overcloud_install.log &> overcloud_install.log
