3a9268fd5a943de7231ff67f96c73b04f0e09d6c
Alex Katz <akatz@redhat.com>
2020-12-07 14:56:30 +0200

    Edit overcloud deployment script in vlan provider network templates
    Change-Id: I29827d91744b6b1775b760d2013579231223dbcd

--- vlan_provider_network/overcloud_deploy.sh
+++ vlan_provider_network/overcloud_deploy.sh
@@ -6,5 +6,4 @@ openstack overcloud deploy \
     --libvirt-type kvm \
     --stack overcloud \
     -r /home/stack/vlan_provider_network/roles/roles_data.yaml \
-    -e /home/stack/vlan_provider_network/roles/nodes.yaml \
-    -e /home/stack/vlan_provider_network/roles/network-config.yaml \
+    -n /home/stack/vlan_provider_network/roles/network-config.yaml \
