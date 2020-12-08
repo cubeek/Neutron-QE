d702965f579cbc56b6b2360f0ca8a20636523850
Alex Katz <akatz@redhat.com>
2020-12-08 14:15:40 +0200

    Fix overcloud deployment script in vlan provider network templates
    Change-Id: I9b6a79263b9f290c7e80a993226a8d169aa82ac9

--- vlan_provider_network/overcloud_deploy.sh
+++ vlan_provider_network/overcloud_deploy.sh
@@ -6,4 +6,8 @@ openstack overcloud deploy \
     --libvirt-type kvm \
     --stack overcloud \
     -r /home/stack/vlan_provider_network/roles/roles_data.yaml \
-    -n /home/stack/vlan_provider_network/network/network-config.yaml
+    -n /home/stack/vlan_provider_network/network/network-config.yaml \
+    -e /usr/share/openstack-tripleo-heat-tempaltes/environments/network-isolation.yaml \
+    -e /usr/share/openstack-tripleo-heat-tempaltes/environments/network-environment.yaml \
+    -e /home/stack/vlan_provider_network/network/network-environment-overrides.yaml \
+    -e /home/stack/vlan_provider_network/roles/nodes.yaml
