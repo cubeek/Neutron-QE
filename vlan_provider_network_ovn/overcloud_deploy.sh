4bd693f9966f34d92f90987dee201a35555ad485
Eduardo Olivares <eolivare@redhat.com>
2021-01-25 18:16:20 +0100

    Added vlan_provider_network_ovn templates
    These templates will be used by the OVN FFU VLAN Provider network job
    
    Change-Id: Ie48bcad5b6c4fd4838fd0d10ba2d71f55b65d77e

---
+++ vlan_provider_network_ovn/overcloud_deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+openstack overcloud deploy \
+--timeout 240 \
+--templates /usr/share/openstack-tripleo-heat-templates \
+--libvirt-type kvm \
+--stack overcloud \
+-r /home/stack/vlan_provider_network_ovn/roles/roles_data.yaml \
+-n /home/stack/vlan_provider_network_ovn/network/network-config.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/cinder-backup.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovn-dvr-ha.yaml \
+-e /home/stack/vlan_provider_network_ovn/network/network-environment.yaml \
+-e /home/stack/vlan_provider_network_ovn/roles/nodes.yaml \
+-e /home/stack/vlan_provider_network_ovn/ovn-extras.yaml \
+-e /home/stack/vlan_provider_network_ovn/l3_fip_qos.yaml \
+-e /home/stack/vlan_provider_network_ovn/docker-images.yaml \
+-e /home/stack/vlan_provider_network_ovn/performance.yaml \
+-e /home/stack/vlan_provider_network_ovn/debug.yaml \
+-e /home/stack/vlan_provider_network_ovn/hostnames.yaml
+
